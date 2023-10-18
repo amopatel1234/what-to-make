481686f2e7b6643fa47cb23865639f1bffc3bfe4
Amish Patel <amopatel1234@gmail.com>
2023-10-18 23:09:52 +0100

    test: created Recipe class

--- Sources/Models/Recipe.swift
+++ Sources/Models/Recipe.swift
@@ -6,7 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
 
 class Recipe {
+    var name: String
+    var timesUsed: Int
+    var servingSize: Int
+    var dateCreated: Date
+    var headerImage: Image
     
+    init(name: String, 
+         timesUsed: Int,
+         servingSize: Int,
+         dateCreated: Date,
+         headerImage: Image) {
+        self.name = name
+        self.timesUsed = timesUsed
+        self.servingSize = servingSize
+        self.dateCreated = dateCreated
+        self.headerImage = headerImage
+    }
 }
--- Tests/RecipeTests.swift
+++ Tests/RecipeTests.swift
@@ -7,32 +7,27 @@
 
 import XCTest
 @testable import whattomake
+import SwiftUI
 
 final class RecipeTests: XCTestCase {
 
     var sut: Recipe!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        sut = Recipe()
+        sut = createMockRecipe()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
+    
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+extension RecipeTests {
+    func createMockRecipe() -> Recipe {
+        Recipe(name: "Pasta",
+               timesUsed: 0,
+               servingSize: 4,
+               dateCreated: Date(timeIntervalSince1970: 200),
+               headerImage: Image(systemName: "eraser"))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
